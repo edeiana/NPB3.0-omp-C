dfde562828463256620c6385f32c0bc5a6d347b2
Enrico Deiana <ead@u.northwestern.edu>
2022-10-26 07:45:41 -0500

    Added burnP6 to run.sh

--- run.sh
+++ run.sh
@@ -25,6 +25,13 @@ rm -f ${timeFile} ;
 outputFile="output.txt" ;
 rm -f ${outputFile} ;
 
+killBurnP6.sh ;
+
+# Note: taskset core list is currently for piraat
+runBurnP6.sh 25 27 29 31 33 35 37 39 41 43 45 47;
+
 for i in $(seq 1 1 ${numOfRuns}) ; do
   export OMP_NUM_THREADS=12 && /usr/bin/time --output=${timeFile} --append --format=%e taskset -c 1,3,5,7,9,11,13,15,17,19,21,23 ./../../bin/${binaryToRun} &>> ${outputFile} ;
 done
+
+killBurnP6.sh ;
