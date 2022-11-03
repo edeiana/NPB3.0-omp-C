78105629eedcf0db8470ba8fb76c175cbee97930
Enrico Deiana <ead@u.northwestern.edu>
2022-11-03 16:22:44 -0500

    Added burnP6.

--- run.sh
+++ run.sh
@@ -29,20 +29,27 @@ rm -f ${timeFile} ;
 outputFile="output.txt" ;
 rm -f ${outputFile} ;
 
-#killBurnP6.sh ;
+# fix.cs.northwestern.edu cores list
+coresList=(56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100 102 104 106 108 110 57 59 61 63 65 67 69 71 73 75 77 79 81 83 85 87 89 91 93 95 97 99 101 103 105 107 109 111)
 
-# Note: taskset core list is currently for piraat
-#runBurnP6.sh 25 27 29 31 33 35 37 39 41 43 45 47;
-#runBurnP6.sh 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100 102 104 106 108 110 ;
-#runBurnP6.sh 57 59 61 63 65 67 69 71 73 75 77 79 81 83 85 87 89 91 93 95 97 99 101 103 105 107 109 111 ;
+# Kill previous burnP6, just in case
+killBurnP6.sh ;
 
+# Run burnP6 on unused cores
+machineCores=${#coresList[@]} ;
+if [ machineCores > numOfCores ] ; then
+  remainingCores=`echo "${machineCores} - ${numOfCores}" | bc` ;
+  coresToRunBurnP6=${coresList[@]:${numOfCores}:${remainingCores}} ;
+  runBurnP6.sh "${coresToRunBurnP6[@]}" ;
+fi
+
+coresToRunApp=${coresList[@]:0:${numOfCores}} ;
+coresToRunAppAsCommaSeparatedList=$( IFS=','; echo "${coresToRunApp[*]}" ) ;
 for i in $(seq 1 1 ${numOfRuns}) ; do
-  #export OMP_NUM_THREADS=12 && /usr/bin/time --output=${timeFile} --append --format=%e taskset -c 1,3,5,7,9,11,13,15,17,19,21,23 ./../../bin/${binaryToRun} &>> ${outputFile} ; # piraat
-  
-  export OMP_NUM_THREADS=${numOfCores} && ${gitRepoDir}/bin/${binaryToRun} &> ${outputFile} ; # fix
+  export OMP_NUM_THREADS=${numOfCores} && taskset -c ${coresToRunAppAsCommaSeparatedList} ${gitRepoDir}/bin/${binaryToRun} &> ${outputFile} ;
   time=`cat ${outputFile} | grep "Time in seconds" | awk '{ print $5 }'` ;
   echo "${time}" >> ${timeFile} ;
-
 done
 
-#killBurnP6.sh ;
+# Kill burnP6 processes at the end
+killBurnP6.sh ;
